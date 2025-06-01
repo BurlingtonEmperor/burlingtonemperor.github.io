fd9a7020c21508eb15597d225656389d017f05a3
BurlingtonEmperor <150710529+BurlingtonEmperor@users.noreply.github.com>
2025-05-31 22:28:57 -0400

    fix

--- variables.js
+++ variables.js
@@ -1,7 +1,7 @@
 // User Interface Window
 const userInterfaceWindow = document.getElementById("user-interface-window");
 const passwordForm = document.getElementById("password-form");
-const thePassword = document.getElementById("the-password");
+const thePassword = document.getElementById("password-here");
 const passwordStatus = document.getElementById("password-status");
 
 let ankhKey = 0;
