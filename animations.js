db7ea49c57359160827c79b9ff5219cd6d48c171
BurlingtonEmperor <150710529+BurlingtonEmperor@users.noreply.github.com>
2025-05-31 21:33:37 -0400

    I think the bg image is good enouh.

--- animations.js
+++ animations.js
@@ -1,19 +1,19 @@
-const sky = document.querySelector('.sky');
+// const sky = document.querySelector('.sky');
 
-function createCloud() {
-  const cloud = document.createElement('div');
-  cloud.className = 'cloud';
-  cloud.style.top = `${Math.random() * window.innerHeight}px`;
-  cloud.style.left = `-${Math.random() * 400 + 200}px`;
-  cloud.style.animationDuration = `${40 + Math.random() * 40}s`;
-  cloud.style.opacity = `${0.6 + Math.random() * 0.3}`;
-  cloud.style.transform = `scale(${0.8 + Math.random() * 0.5})`;
-  cloud.style.animationDelay = `${Math.random() * 30}s`;
-  sky.appendChild(cloud);
+// function createCloud() {
+//   const cloud = document.createElement('div');
+//   cloud.className = 'cloud';
+//   cloud.style.top = `${Math.random() * window.innerHeight}px`;
+//   cloud.style.left = `-${Math.random() * 400 + 200}px`;
+//   cloud.style.animationDuration = `${40 + Math.random() * 40}s`;
+//   cloud.style.opacity = `${0.6 + Math.random() * 0.3}`;
+//   cloud.style.transform = `scale(${0.8 + Math.random() * 0.5})`;
+//   cloud.style.animationDelay = `${Math.random() * 30}s`;
+//   sky.appendChild(cloud);
   
-  setTimeout(() => {
-    sky.removeChild(cloud);
-  }, 80000); 
-}
+//   setTimeout(() => {
+//     sky.removeChild(cloud);
+//   }, 80000); 
+// }
 
-setInterval(createCloud, 4000);
+// setInterval(createCloud, 4000);
