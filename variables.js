b82b5389ffdeaeca814bec2f81004f3464661766
BurlingtonEmperor <150710529+BurlingtonEmperor@users.noreply.github.com>
2025-05-31 22:26:53 -0400

    password implementation

--- variables.js
+++ variables.js
@@ -2,3 +2,6 @@
 const userInterfaceWindow = document.getElementById("user-interface-window");
 const passwordForm = document.getElementById("password-form");
 const thePassword = document.getElementById("the-password");
+const passwordStatus = document.getElementById("password-status");
+
+let ankhKey = 0;
