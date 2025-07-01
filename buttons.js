b5c129fcf266770d1d4aa0680126f002ea04fdb6
BurlingtonEmperor <150710529+BurlingtonEmperor@users.noreply.github.com>
2025-07-01 19:28:48 +0900

    fix path

--- buttons.js
+++ buttons.js
@@ -3,7 +3,7 @@ const msmsButton = document.getElementById("msms-button");
 const bhsButton = document.getElementById("bhs-button");
 
 pineglenButton.onclick = function () {
-  animPut(homescreen, "/PinewoodChapel/pineglen.mp3", "sunrise.webp", pineglenDiv);
+  animPut(homescreen, "/PinewoodChapel/pineglen.mp3", "/PinewoodChapel/images/sunrise.webp", pineglenDiv);
 }
 
 msmsButton.onclick = function () {
