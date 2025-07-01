e1badd13decf31dacf7b1053f9bc4e416b59f458
BurlingtonEmperor <150710529+BurlingtonEmperor@users.noreply.github.com>
2025-07-01 16:41:44 +0900

    load smoothly

--- animations.js
+++ animations.js
@@ -39,6 +39,34 @@ function loadBack (backSrc) {
   newImg.src = backSrc;
 }
 
+function waitForElement(querySelector, timeout){
+  return new Promise((resolve, reject)=>{
+    var timer = false;
+    if(document.querySelectorAll(querySelector).length) return resolve();
+    const observer = new MutationObserver(()=>{
+      if(document.querySelectorAll(querySelector).length){
+        observer.disconnect();
+        if(timer !== false) clearTimeout(timer);
+        return resolve();
+      }
+    });
+    observer.observe(document.body, {
+      childList: true, 
+      subtree: true
+    });
+    if(timeout) timer = setTimeout(()=>{
+      observer.disconnect();
+      reject();
+    }, timeout);
+  });
+}
+
+waitForElement("body", 3000).then(function () {
+  loadBack("/PinewoodChapel/images/sky.jpg");
+}).catch(() => {
+  console.log("Error: did not load!");
+});
+
 function openTheGates () {
   $(userInterfaceWindow).slideUp();
   audio95.play();
