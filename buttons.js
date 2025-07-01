d8ede14103f43ac1b424cb30c17ffa9027b573a4
BurlingtonEmperor <150710529+BurlingtonEmperor@users.noreply.github.com>
2025-07-01 19:08:39 +0900

    smoothen things out

---
+++ buttons.js
@@ -0,0 +1,15 @@
+const pineglenButton = document.getElementById("pineglen-button");
+const msmsButton = document.getElementById("msms-button");
+const bhsButton = document.getElementById("bhs-button");
+
+pineglenButton.onclick = function () {
+  
+}
+
+msmsButton.onclick = function () {
+
+}
+
+bhsButton.onclick = function () {
+
+}
