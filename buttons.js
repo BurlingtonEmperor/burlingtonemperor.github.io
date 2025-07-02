21ec0364705c11f9a3982b64a77b80e7289b8eb4
BurlingtonEmperor <150710529+BurlingtonEmperor@users.noreply.github.com>
2025-07-02 16:54:04 +0900

    add new buttons and sounds

--- buttons.js
+++ buttons.js
@@ -2,6 +2,11 @@ const pineglenButton = document.getElementById("pineglen-button");
 const msmsButton = document.getElementById("msms-button");
 const bhsButton = document.getElementById("bhs-button");
 
+// back buttons
+const backPineglen = document.getElementById("back-pineglen");
+const backMsms = document.getElementById("back-msms");
+const backBhs = document.getElementById("back-bhs");
+
 pineglenButton.onclick = function () {
   animPut(homescreen, "/PinewoodChapel/pineglen.mp3", "/PinewoodChapel/images/morning.jpg", pineglenDiv);
 }
@@ -12,4 +17,17 @@ msmsButton.onclick = function () {
 
 bhsButton.onclick = function () {
   animPut(homescreen, "/PinewoodChapel/PS2.mp3", "/PinewoodChapel/images/night.jpg", bhsDiv);
+}
+
+// back clicks
+backPineglen.onclick = function () {
+  animPut(pineglenDiv, "/PinewoodChapel/Utopia_Asterisk.mp3", "/PinewoodChapel/images/pinksky.jpeg", homescreen);
+}
+
+backMsms.onclick = function () {
+  animPut(msmsDiv, "/PinewoodChapel/windowsnt.mp3", "/PinewoodChapel/images/pinksky.jpeg", homescreen);
+}
+
+backBhs.onclick = function () {
+  animPut(bhsDiv, "/PinewoodChapel/PSX.mp3", "/PinewoodChapel/images/pinksky.jpeg", homescreen);
 }
