897444547074d7efc38ecc3e0e74d726fa529445
BurlingtonEmperor <150710529+BurlingtonEmperor@users.noreply.github.com>
2025-07-12 09:31:36 +0900

    Update mobile.js

--- mobile.js
+++ mobile.js
@@ -15,5 +15,7 @@ if (window.mobileCheck() === true) {
   guestbookIframe.src = "https://pinewoodchapel.netlify.app/mobile.html";
   body.style.overflowX = "hidden";
   guestbookIframe.style.overflowX = "hidden";
+  
+  siteNewsFont.style.fontSize = "3.5vw";
   // guestbookIframe.style.width = "500px";
 }
