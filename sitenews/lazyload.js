d33312e547c2ba24ff3d2a357736130685516cb8
BurlingtonEmperor <150710529+BurlingtonEmperor@users.noreply.github.com>
2025-07-12 15:19:28 +0900

    Update lazyload.js

--- sitenews/lazyload.js
+++ sitenews/lazyload.js
@@ -15,3 +15,41 @@ document.addEventListener("DOMContentLoaded", () => {
 
   lazyDivs.forEach(div => observer.observe(div));
 });
+
+function waitForElement(querySelector, timeout){
+  return new Promise((resolve, reject)=>{
+    var timer = false;
+    if(document.querySelectorAll(querySelector).length) return resolve();
+    const observer = new MutationObserver(()=>{
+      if(document.querySelectorAll(querySelector).length){
+        observer.disconnect();
+        if(timer !== false) clearTimeout(timer);
+        return resolve();
+      }
+    });
+    observer.observe(document.body, {
+      childList: true, 
+      subtree: true
+    });
+    if(timeout) timer = setTimeout(()=>{
+      observer.disconnect();
+      reject();
+    }, timeout);
+  });
+}
+
+const body = document.body;
+function loadBack (backSrc) {
+  let newImg = new Image();
+
+  newImg.onload = function () {
+    body.style.backgroundImage = "url('" + newImg.src + "')";
+  }
+  newImg.src = backSrc;
+}
+
+waitForElement("body", 3000).then(function () {
+  loadBack("clouds.jpg");
+}).catch(() => {
+  console.log("Error: did not load!");
+});
